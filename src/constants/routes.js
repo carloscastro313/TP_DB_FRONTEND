5677b1e5e350dd99b74b46a166710e311e832c41
castrocarlos313 <castrocarlos313@gmail.com>
2022-06-03 20:46:48 -0300

    V2

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,43 +1,73 @@
 const routes = [
   {
-    route: "/",
-    name: "Obtener todos los tickets",
+    route: "/reclamos",
+    name: "Obtener todos los reclamos",
     id: 1,
+  },
+  {
+    route: "/planes",
+    name: "Obtener todos los planes",
+    id: 2,
+  },
+  {
+    route: "/areas",
+    name: "Obtener todas las areas",
+    id: 3,
+  },
+  {
+    route: "/usuarios",
+    name: "Obtener todos los usuarios",
+    id: 4,
   },
   {
     route: "/desperfectos",
     name: "Obtener todos los desperfectos",
-    id: 2,
+    id: 5,
   },
   {
     route: "/desperfectosPorZona",
-    name: "Obtener todos los tickets por zona",
-    id: 3,
+    name: "Obtener todos los reclamos por zona",
+    id: 6,
   },
   {
     route: "/atenciones",
     name: "Obtener todos las atenciones",
-    id: 4,
+    id: 7,
   },
   {
     route: "/atencionPorZona",
     name: "Obtener todos las atenciones por zona",
-    id: 5,
+    id: 8,
   },
   {
     route: "/mayorAtendidos",
-    name: "El area con mayor cantidad de tickets",
-    id: 6,
+    name: "El area con mayor cantidad de reclamos",
+    id: 9,
   },
   {
     route: "/ticketsSinResolver",
-    name: "Obtener tickets sin resolver",
-    id: 7,
+    name: "Obtener reclamos sin resolver",
+    id: 10,
   },
   {
     route: "/atencionPorTipo",
     name: "Obtener cantidad de baja, altas y cambios de plan",
-    id: 8,
+    id: 11,
+  },
+  {
+    route: "/coberturaLanus",
+    name: "Usuario en el area de cobertura de servicio tecnico - lanus",
+    id: 12,
+  },
+  {
+    route: "/usuarioAvellaneda",
+    name: "Usuario en la zona de avellaneda",
+    id: 13,
+  },
+  {
+    route: "/sinCobertura",
+    name: "Usuario sin cobertura",
+    id: 13,
   },
 ];
 
